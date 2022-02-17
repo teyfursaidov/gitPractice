cfb9c27cf2eb0388c6f57aac9dd83e452f96327f
teyfursaidov <teyfursaidov1991@gmail.com>
2022-02-16 18:59:24 -0700

    This commit

---
+++ src/git/batch12.java
@@ -0,0 +1,31 @@
+package git;
+
+public class batch12 {
+
+	public static void main(String[] args) {
+
+
+		
+		
+		System.out.println("We the best.");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
